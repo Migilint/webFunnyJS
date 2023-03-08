e6e1260dd98e03b175defe8799fc43b44a6a48ff
Migilint <112771641+Migilint@users.noreply.github.com>
2023-03-08 11:27:15 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import {colors, board} from './photoArray.js';
 const SQUARES_NUMBER = 900
+const activeFun = 600;
 
 const displayWidth0 = window.innerWidth;
 const displayHeight0 = window.innerHeight;
@@ -37,14 +38,14 @@ function setColor (element, i) {
 function autoMargin() {
     var displayWidth = window.innerWidth;
     const displayHeight = window.innerHeight;
-    const marginWidth = (displayWidth - 600) / 2
-    const marginHeight = (displayHeight - 600) / 2
+    const marginWidth = (displayWidth - activeFun) / 2
+    const marginHeight = (displayHeight - activeFun) / 2
 
     board.style.margin = `${marginHeight}px ${marginWidth}px`;
 }
 
 function conditionCounts() {
-    if ((document.querySelectorAll('.square').length >= 600 && 
+    if ((document.querySelectorAll('.square').length >= activeFun && 
         document.querySelectorAll('.active').length == 10)) {
         //console.log('Transition! :)'); alert("Transition...");
         window.location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
